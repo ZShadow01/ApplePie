3e2a324e6f81eb2edbf2b55c107182072c22381f
ZShadow01 <shad.aeons@gmail.com>
2024-11-27 15:21:14 +0100

    Fixed error code and metadata mix up

--- shared/errors/database/DatabaseDuplicateEntryError.js
+++ shared/errors/database/DatabaseDuplicateEntryError.js
@@ -3,6 +3,7 @@ const DatabaseError = require('./DatabaseError');
 
 module.exports = class DatabaseDuplicateEntryError extends DatabaseError {
     constructor(key) {
-        super('Duplicate entry detected', 'DUPLICATE_ENTRY_ERROR', { key });
+        super('Duplicate entry detected', { key });
+        this.code = 'DUPLICATE_ENTRY_ERROR';
     }
 };
