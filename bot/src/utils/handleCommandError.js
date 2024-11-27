6935731d34abdb74a7aa73cf932e8530399d5314
ZShadow01 <shad.aeons@gmail.com>
2024-11-27 15:22:04 +0100

    Added handleCommandError function to handle errors on the Discord bot client side

---
+++ bot/src/utils/handleCommandError.js
@@ -0,0 +1,32 @@
+const BaseError = require('../../../shared/errors/BaseError');
+
+const { error_colour } = require('../../config/default.json');
+
+
+module.exports = async function handleCommandError(error, interaction) {
+    const embed = {
+        color: error_colour
+    };
+
+    if (error instanceof BaseError) {
+        switch (error.code) {
+            case 'PLAYER_ALREADY_REGISTERED':
+                embed.description = 'You are already registered as a player';
+                break;
+
+            default:
+                embed.description = `An error occurred: ${error.code}`;
+                break;
+        }
+    } else {
+        console.error(error);
+        embed.description = 'Uh oh, something went wrong';
+    }
+
+    if (interaction.replied || interaction.deferred) {
+        await interaction.followUp({ embeds: [embed] });
+        return;
+    }
+
+    await interaction.reply({ embeds: [embed] });
+};
