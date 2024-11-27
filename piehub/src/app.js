1e22b0f7aa89cdb9c7b0edbdd4fcfc6bdadea01c
ZShadow01 <shad.aeons@gmail.com>
2024-11-27 04:05:49 +0100

    Let express app use the API router

--- piehub/src/app.js
+++ piehub/src/app.js
@@ -1,10 +1,10 @@
 const express = require('express');
-
+const apiRoutes = require('./api/routes');
 
 const app = express();
 
 app.use(express.json());
-
+app.use('/api/', apiRoutes);
 
 app.use((req, res, next) => {
     res.status(404).json({ error: 'Not Found' });
