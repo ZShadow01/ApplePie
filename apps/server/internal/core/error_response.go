e57bdc292fc3e6fc41232a3a5f840bc5d4253e27
ZShadow01 <shad.aeons@gmail.com>
2025-04-12 22:35:35 +0200

    Add constants and enums for error responses

--- apps/server/internal/core/error_response.go
+++ apps/server/internal/core/error_response.go
@@ -1,8 +1,21 @@
 package core
 
+type ErrorCode int
+
+const (
+	ErrMissingArguments         = 100
+	ErrInternalServer           = 101
+	ErrPlayerDuplicateUsername  = 110
+	ErrPlayerDuplicateDiscordID = 111
+)
+
+type ErrorResponseDetails struct {
+	Field string `json:"field,omitempty"`
+}
+
 type ErrorResponse struct {
-	Error   string `json:"error"`
-	Message string `json:"message"`
-	Field   string `json:"field,omitempty"`
-	Code    string `json:"code"`
+	StatusCode int                  `json:"status"`
+	Code       int                  `json:"code"`
+	Message    string               `json:"message"`
+	Details    ErrorResponseDetails `json:"details,omitempty"`
 }
