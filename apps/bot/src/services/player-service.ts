5c64b947cc8cca32c6d425403ba3362cb0b7a9f8
ZShadow01 <shad.aeons@gmail.com>
2025-04-12 22:34:15 +0200

    switch-case application-level error codes

--- apps/bot/src/services/player-service.ts
+++ apps/bot/src/services/player-service.ts
@@ -1,21 +1,48 @@
 import ServerConnector from '../core/server-connector.js';
 import { config } from 'dotenv';
+import NameTakenError from '../errors/name-taken-error.js';
+import APIError from '../errors/api-error.js';
+import AlreadyExistsError from '../errors/already-exists-error.js';
+import UnknownError from '../errors/unknown-error.js';
 
 config();
 
 const API_URL = `${process.env['SERVER_URL']}/api/v1/players`;
 
-export async function registerPlayer(discordID: string, username: string) {
-    const res = await ServerConnector.request(`${API_URL}/register`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            discordID,
-            username,
-        }),
-    });
+export async function registerPlayer(discordId: string, username: string) {
+    try {
+        await ServerConnector.request(`${API_URL}/register`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                discordId,
+                username,
+            }),
+        });
+    } catch (err) {
+        if (err instanceof APIError) {
+            switch (err.code) {
+                case 110:
+                    throw new NameTakenError(
+                        `The player name '**${username}**' is taken`,
+                        username
+                    );
 
-    console.log(res);
+                case 111:
+                    throw new AlreadyExistsError(
+                        `You are already registered as a player`,
+                        discordId
+                    );
+
+                case 101:
+                    throw new UnknownError(
+                        'An unexpected server error occurred while registering you as a player'
+                    );
+            }
+        }
+
+        throw err;
+    }
 }
