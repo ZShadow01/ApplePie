9a59b3c68ec03f5fc32a1f8351adb9890d6c9b57
ZShadow01 <shad.aeons@gmail.com>
2025-04-12 02:57:45 +0200

    Added ServerConnector - bridge to the REST API server

---
+++ apps/bot/src/core/server-connector.ts
@@ -0,0 +1,31 @@
+import UnknownError from '../errors/unknown-error.js';
+import HttpError from '../errors/http-error.js';
+import APIError from '../errors/api-error.js';
+
+export default class ServerConnector {
+    constructor() {
+        // TODO: websocket/socket.io connection to the server
+    }
+
+    static async request(url: string, options?: RequestInit) {
+        let res;
+
+        try {
+            res = await fetch(url, options);
+        } catch (err) {
+            if (err instanceof TypeError) {
+                throw new HttpError('Unable to reach the server');
+            }
+
+            throw new UnknownError();
+        }
+
+        const json = await res.json();
+
+        if (!res.ok) {
+            throw new APIError(json['message'], json['code']);
+        }
+
+        return json;
+    }
+}
