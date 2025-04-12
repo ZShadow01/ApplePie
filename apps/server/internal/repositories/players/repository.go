cc6e41e9b2cf4ee88a1dff17c1779973efab8fd5
ZShadow01 <shad.aeons@gmail.com>
2025-04-12 22:36:05 +0200

    Implemented NewPlayer

--- apps/server/internal/repositories/players/repository.go
+++ apps/server/internal/repositories/players/repository.go
@@ -1,14 +1,67 @@
 package playerrepository
 
 import (
+	"fmt"
+	"log"
+	"net/http"
+	"regexp"
+
+	"github.com/ZShadow01/ApplePie/apps/server/internal/core"
 	"github.com/ZShadow01/ApplePie/apps/server/internal/db"
+	"github.com/go-sql-driver/mysql"
 	"github.com/google/uuid"
 )
 
-func NewPlayer(discordID uint64, username string) error {
+var duplicateEntryRegex = regexp.MustCompile(`for key '([^']+)'`)
+
+func NewPlayer(discordID string, username string) *core.ErrorResponse {
 	id := uuid.New()
 
 	_, err := db.Exec("INSERT INTO players (uuid, discord_id, username) VALUES(?, ?, ?)", id[:], discordID, username)
 
-	return err
+	if err != nil {
+		mysqlError, ok := err.(*mysql.MySQLError)
+
+		if !ok {
+			log.Printf("Player Registration Error: %v", err)
+			return &core.ErrorResponse{
+				StatusCode: http.StatusInternalServerError,
+				Code: core.ErrInternalServer,
+				Message: "An unexpected error occurred while registering the user",
+			}
+		}
+
+		// Duplicate Entry
+		if mysqlError.Number == 1062 {
+			match := duplicateEntryRegex.FindStringSubmatch(mysqlError.Message)
+	
+			if len(match) > 1 {
+				conflictingKey := match[1]
+
+				var code int
+				if conflictingKey == "discord_id" {
+					code = core.ErrPlayerDuplicateDiscordID
+				} else if conflictingKey == "username" {
+					code = core.ErrPlayerDuplicateUsername
+				}
+
+				return &core.ErrorResponse{
+					StatusCode: http.StatusConflict,
+					Code: code,
+					Message: fmt.Sprintf("A player with this %s already exists", conflictingKey),
+					Details: core.ErrorResponseDetails{
+						Field:conflictingKey,
+					},
+				}
+			}
+		}
+
+		return &core.ErrorResponse{
+			StatusCode: http.StatusInternalServerError,
+			Code: core.ErrInternalServer,
+			Message: "An unexpected error occurred while registering the user",
+		}
+	}
+
+	return nil
 }
